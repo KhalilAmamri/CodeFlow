caf70d4b9cfe88f0f0c288204bf3b8eadc649b2e
KhalilAmamri <KhalilAmamri@outlook.com>
2025-08-18 04:40:08 +0100

    create template

--- pythonic.py
+++ pythonic.py
@@ -1,14 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+lessons = [{
+    "title": "Request Library Course",
+    "course": "Python",
+    "author" : "Khalil"
+},
+{
+    "title": "Request-HTML Library Course",
+    "course": "Python",
+    "author" : "Omar"
+},
+{
+    "title": "Datetime Module",
+    "course": "Python",
+    "author" : "mahdi"
+}
+]
+
 @app.route("/")
 def home():
-    return "<h1> Hom Page </h1>"
+    return render_template('home.html', lessons = lessons)
 
 @app.route("/about")
 def about():
-    return "<h1> About Page </h1>"
+    return render_template('about.html', title = "About Page")
 
 if __name__== "__main__":
     app.run(debug=True)
