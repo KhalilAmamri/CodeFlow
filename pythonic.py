00a64c3092d26e7532f477bf71fa46e01b756ec7
KhalilAmamri <KhalilAmamri@outlook.com>
2025-08-18 19:35:53 +0100

    Fixed Jinja2 template syntax error and added missing courses data to enable proper image display on the home page.

--- pythonic.py
+++ pythonic.py
@@ -3,25 +3,79 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 lessons = [{
-    "title": "Request Library Course",
-    "course": "Python",
-    "author" : "Khalil"
+    'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'Omar',
+    'thumbnail': 'thumbnail.jpg'
 },
-{
-    "title": "Request-HTML Library Course",
-    "course": "Python",
-    "author" : "Omar"
+{'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'Omar',
+    'thumbnail': 'thumbnail.jpg'
 },
+{'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'Omar',
+    'thumbnail': 'thumbnail.jpg'
+},
+{'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'Omar',
+    'thumbnail': 'thumbnail.jpg'
+},
+{'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'Omar',
+    'thumbnail': 'thumbnail.jpg'
+},
+{'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'Omar',
+    'thumbnail': 'thumbnail.jpg'
+},
+]
+courses = [
 {
-    "title": "Datetime Module",
-    "course": "Python",
-    "author" : "mahdi"
-}
+        'name': 'Python',
+        'icon': 'python.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+    {
+        'name': 'Data Analysis',
+        'icon': 'analysis.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+    {
+        'name': 'Machine Learning',
+        'icon': 'machine-learning.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+        {
+        'name': 'Web Design',
+        'icon': 'web.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+        {
+        'name': 'Blockchain',
+        'icon': 'blockchain.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+        {
+        'name': 'Tips & Tricks',
+        'icon': 'idea.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
 ]
 
 @app.route("/")
 def home():
-    return render_template('home.html', lessons = lessons)
+    return render_template('home.html', lessons = lessons, courses = courses)
 
 @app.route("/about")
 def about():
