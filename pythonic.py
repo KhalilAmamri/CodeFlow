7f2935429f99dcbc5a627edabe831d3fb635d1e3
KhalilAmamri <KhalilAmamri@outlook.com>
2025-08-18 02:56:49 +0100

    add git ignore file

--- pythonic.py
+++ pythonic.py
@@ -1,2 +1,14 @@
 from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1> Hom Page </h1>"
+
+@app.route("/about")
+def about():
+    return "<h1> About Page </h1>"
+
+if __name__== "__main__":
+    app.run(debug=True)
