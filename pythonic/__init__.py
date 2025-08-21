be26c93ebafed88ea83f5e029b9d3252b0973ad9
KhalilAmamri <KhalilAmamri@outlook.com>
2025-08-21 20:46:37 +0100

    `Added bcrypt for password hashing and updated routes to use it`

--- pythonic/__init__.py
+++ pythonic/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
-
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
@@ -9,5 +9,7 @@ app.config['SECRET_KEY'] = 'e08fe17fe27ffc7009aba6a264a886ea81a5b5bd17f1fc97b955
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pythonic.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
 
 from pythonic import routes
--- pythonic/routes.py
+++ pythonic/routes.py
@@ -1,103 +1,125 @@
 from pythonic.models import User, Lesson, Course
 from flask import render_template, url_for, flash, redirect
 from pythonic.forms import RegistrationForm, LoginForm
-from pythonic import app
+from pythonic import app, db, bcrypt
 
-lessons = [{
-    'title': 'Request Library Course',
-    'course': 'Python',
-    'author': 'Omar',
-    'thumbnail': 'thumbnail.jpg'
-},
-{'title': 'Request Library Course',
-    'course': 'Python',
-    'author': 'Omar',
-    'thumbnail': 'thumbnail.jpg'
-},
-{'title': 'Request Library Course',
-    'course': 'Python',
-    'author': 'Omar',
-    'thumbnail': 'thumbnail.jpg'
-},
-{'title': 'Request Library Course',
-    'course': 'Python',
-    'author': 'Omar',
-    'thumbnail': 'thumbnail.jpg'
-},
-{'title': 'Request Library Course',
-    'course': 'Python',
-    'author': 'Omar',
-    'thumbnail': 'thumbnail.jpg'
-},
-{'title': 'Request Library Course',
-    'course': 'Python',
-    'author': 'Omar',
-    'thumbnail': 'thumbnail.jpg'
-},
+lessons = [
+    {
+        "title": "Request Library Course",
+        "course": "Python",
+        "author": "Omar",
+        "thumbnail": "thumbnail.jpg",
+    },
+    {
+        "title": "Request Library Course",
+        "course": "Python",
+        "author": "Omar",
+        "thumbnail": "thumbnail.jpg",
+    },
+    {
+        "title": "Request Library Course",
+        "course": "Python",
+        "author": "Omar",
+        "thumbnail": "thumbnail.jpg",
+    },
+    {
+        "title": "Request Library Course",
+        "course": "Python",
+        "author": "Omar",
+        "thumbnail": "thumbnail.jpg",
+    },
+    {
+        "title": "Request Library Course",
+        "course": "Python",
+        "author": "Omar",
+        "thumbnail": "thumbnail.jpg",
+    },
+    {
+        "title": "Request Library Course",
+        "course": "Python",
+        "author": "Omar",
+        "thumbnail": "thumbnail.jpg",
+    },
 ]
 courses = [
-{
-        'name': 'Python',
-        'icon': 'python.png',
-        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
-    },
-
     {
-        'name': 'Data Analysis',
-        'icon': 'analysis.png',
-        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+        "name": "Python",
+        "icon": "python.png",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!",
     },
-
     {
-        'name': 'Machine Learning',
-        'icon': 'machine-learning.png',
-        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+        "name": "Data Analysis",
+        "icon": "analysis.png",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!",
     },
-
-        {
-        'name': 'Web Design',
-        'icon': 'web.png',
-        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    {
+        "name": "Machine Learning",
+        "icon": "machine-learning.png",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!",
     },
-
-        {
-        'name': 'Blockchain',
-        'icon': 'blockchain.png',
-        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    {
+        "name": "Web Design",
+        "icon": "web.png",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!",
     },
-
-        {
-        'name': 'Tips & Tricks',
-        'icon': 'idea.png',
-        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    {
+        "name": "Blockchain",
+        "icon": "blockchain.png",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!",
     },
-
+    {
+        "name": "Tips & Tricks",
+        "icon": "idea.png",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!",
+    },
 ]
+
 
 @app.route("/")
 def home():
-    return render_template('home.html', lessons = lessons, courses = courses)
+    return render_template("home.html", lessons=lessons, courses=courses)
+
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title = "About Page")
+    return render_template("about.html", title="About Page")
 
-@app.route("/register", methods=['GET', 'POST'])
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f"Account created for {form.first_name.data} {form.last_name.data}!", 'success')
-        return redirect(url_for('home'))
-        
-    return render_template('register.html', title = "Register", form = form)
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode(
+            "utf-8"
+        )
+        user = User(
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            username=form.username.data,
+            email=form.email.data,
+            password=hashed_password,
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        flash(
+            f"Account created for {form.first_name.data} {form.last_name.data}!",
+            "success",
+        )
+        return redirect(url_for("home"))
+
+    return render_template("register.html", title="Register", form=form)
 
 
-@app.route("/login", methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         # add a simple test for login with hardcoded credentials
-        if form.email.data == "khalilamamri@gmail.com" and form.password.data == "password123":
-            flash("Login successful!", 'success')
-            return redirect(url_for('home'))
-    return render_template('login.html', title = "Login", form = form)
+        if (
+            form.email.data == "khalilamamri@gmail.com"
+            and form.password.data == "password123"
+        ):
+            flash("Login successful!", "success")
+            return redirect(url_for("home"))
+    return render_template("login.html", title="Login", form=form)
